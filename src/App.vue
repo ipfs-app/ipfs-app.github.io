f04eccde3e9ce367862c4c70de309813414904bc
bill080307 <bill080307@live.cn>
2025-10-29 16:32:55 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,75 @@
+<template>
+<div>
+  <nav id="navbar" class="navbar navbar-expand-lg bg-body-tertiary bg-dark sticky-top" data-bs-theme="dark">
+    <div class="container">
+      <a class="navbar-brand" href="#">Home</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link" href="#ipfs">IPFS服务</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#infrastructure">基础设施服务</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <div data-bs-spy="scroll" data-bs-target="#navbar" data-bs-root-margin="0px 0px -40%" data-bs-smooth-scroll="true" class="container scrollspy-example bg-body-tertiary p-3 rounded-2" tabindex="0">
+  <!-- Come Soon 提示 -->
+  <div class="text-center py-12">
+    <h2 class="text-3xl font-bold text-primary">Coming Soon</h2>
+    <p class="mt-2 text-muted">We are working on exciting new features!</p>
+  </div>
+  <p>...</p>
+  <p>...</p>
+  <p>...</p>
+  <p>...</p>
+  <p>...</p>
+  <p>...</p>
+  <p>...</p>
+  <p>...</p>
+  <p>...</p>
+  <p>...</p>
+  <p>...</p>
+  <p>...</p>
+  <p>...</p>
+  <p>...</p>
+  <p>...</p>
+  <p>...</p>
+  <p>...</p>
+  <h4 id="ipfs">IPFS服务</h4>
+  <p>...</p>
+  <p>...</p>
+  <p>...</p>
+  <p>...</p>
+  <p>...</p>
+  <p>...</p>
+  <p>...</p>
+  <p>...</p>
+  <p>...</p>
+  <p>...</p>
+  <p>...</p>
+  <h4 id="infrastructure">基础设施服务</h4>
+  <p>...</p>
+  <p>...</p>
+  <p>...</p>
+  <p>...</p>
+  <p>...</p>
+  <p>...</p>
+  <p>...</p>
+  <p>...</p>
+  <p>...</p>
+  <p>...</p>
+    </div>
+</div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+</style>
